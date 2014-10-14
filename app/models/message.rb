ef991a9d6dab11173043f74177ced4dd57d14477
Lex <l.alexander10@gmail.com>
2014-10-13 17:21:04 -0700

    well I fixed a yaml error and just installed figaro.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,16 +1,16 @@
 require "twilio-ruby"
 class Message < ActiveRecord::Base
-
-	def send_text
-		twilio_sid = ENV["twilio_sid"]
-		twilio_token = ENV["twilio_token"]
+	include ApplicationHelper
+		def send_text
+		twilio_sid = ENV["TWILIO_SID"]
+		twilio_token = ENV["TWILIO_TOKEN"]
 		twilio_phone_number = "+18182769377"
 		twilio_url = "http://twimlets.com/callme?PhoneNumber=6266443347&Message=HI%20HOW%20ARE%20YOU%3F%20&FailUrl=%22http%3A%2F%2Ftwimlets.com%2Fvoicemail%3FEmail%3Dworkemaillex%40gmail.com%26Message%3DPlease%2BLeave%2BA%2BMessage%22&"
 		# rememeber this: the scope of a variable within a method is limited to that 
 		# method unless you definie it globally. So you couldn't access the 
 		# the twilio token or id because it was defined outside of this method
 		# however the class aka message could use it 
-		@client = Twilio::REST::Client.new("AC01b37849cc3e96e4fb12fe5f073ba1a6", "27452efd374fa7a86af0547fd861d464")
+		@client = Twilio::REST::Client.new(ENV["TWILIO_SID"],  ENV["TWILIO_TOKEN"]) 
 		@message = @client.account.messages.create({
 			:from => twilio_phone_number,
 			:to =>   self.number,
@@ -20,7 +20,6 @@ class Message < ActiveRecord::Base
 
 		 
 	end
-
 
 
 	
