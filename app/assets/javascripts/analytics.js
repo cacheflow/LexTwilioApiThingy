acf3d66ae8218e0eb1b86d114deea17c6729a7cf
Lex <l.alexander10@gmail.com>
2014-10-17 13:09:44 -0700

    refactored the turbolinks workaround for google analytics.

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,22 +1,15 @@
-$('#button').on('click', function() {
-  ga('send', 'event', 'button', 'click', 'nav-buttons');
+$(document).ready(function(){
+	ga('create', 'UA-50962525-1', 'localhost:3000')
+
+
+}); 
+
+
+$("#message_button").click(function(){
+
+
+	ga("send", "event", "Messages", "Sent");
+
+
 });
 
-
-
-// $(document).on('page:change', function() {
-//  if (window._gaq != null) {
-//   return _gaq.push(['_trackPageview']);
-//  } else if (window.pageTracker != null) {
-//   return pageTracker._trackPageview();
-//  }
-// });
-
-
-
-// //   @trackPageview: (url) ->
-// //       if url
-// //         window._gaq.push ["_trackPageview", url]
-// //       else
-// //         window._gaq.push ["_trackPageview"]
-// //       window._gaq.push ["_trackPageLoadTime"]
